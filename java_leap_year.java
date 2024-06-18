47d30886de588c4bbdc5c6abb82c095394decb15
sohail262 <shaiksabira90@gmail.com>
2024-06-18 23:45:01 +0530

    file renamed

--- java_leap_year.java
+++ java_leap_year.java
@@ -5,9 +5,9 @@ public class java_leap_year{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int year= sc.nextInt();
-        boolean x=(year%4)==0;
-        boolean y=(year%100)!=0;
-        boolean z=((year%100==0) && (year%400==0));
+        boolean x=(year%4==0);
+        boolean y=(year%100==0);
+        boolean z=((year&100==0) && (year%400==0));
 
         if(x && (y || z)){
             System.out.println("leap year");
